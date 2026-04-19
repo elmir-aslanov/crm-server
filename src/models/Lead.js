e53d44756456b0c01cfdfa41f2dda62278ecea01
elmir <elmireas@code.edu.az>
2026-04-19 17:47:45 +0400

    lead model update

--- src/models/Lead.js
+++ src/models/Lead.js
@@ -5,22 +5,48 @@ const leadSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
+      trim: true,
     },
     lastName: {
       type: String,
       required: true,
+      trim: true,
     },
     phone: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    courseInterest: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      default: null
+    },
+    source: {
+      type: String,
+      enum: ['Website', 'Phone', 'Facebook', 'Instagram', 'Refarral', 'Walk-in'],
+      default: 'Website',
     },
     status: {
       type: String,
       enum: ['New', 'Contacted', 'Interested', 'Trial', 'Enrolled', 'Lost'],
       default: 'New',
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      default: null,
+    },
+    utmSource:{
+      type: String,
+      trim: true,
+      deafult:null,
     },
   },
   {
